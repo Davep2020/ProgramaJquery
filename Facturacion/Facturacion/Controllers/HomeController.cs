4969110e99f53f91d17f347a1e2df77b51ba6fb2
JOSE DAVID FONSECA ARAYA <119190206@castrocarazo.ac.cr>
2021-10-21 17:42:08 -0600

    Jquery

---
+++ Facturacion/Facturacion/Controllers/HomeController.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Facturacion.Models;
+
+namespace Facturacion.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        FacturaEntities modelo = new FacturaEntities(); 
+
+        public ActionResult Index()
+        {
+            List<MuestraLosCientes_Result> lista = new List<MuestraLosCientes_Result>();
+            lista = modelo.MuestraLosCientes().ToList();
+            return View(lista);
+        }
+
+
+        #region Json
+        public ActionResult RetornaClientes()
+        {
+            List<MuestraLosCientes_Result> Cliente = this.modelo.MuestraLosCientes().ToList();
+            return Json(Cliente);
+        }
+        public ActionResult RetornaFactura(int CodigoCliente)
+        {
+            List<MuestralasFacturasCLIENTE_Result> Factura = this.modelo.MuestralasFacturasCLIENTE(CodigoCliente).ToList();
+            return Json(Factura);
+        }
+
+
+        [HttpPost]
+        public ActionResult Recibos(RecibosSelect_Result modelos,int? MontoAbono1 , int? NumeroFacturaAbonada1, int? MontoAbono2, int? NumeroFacturaAbonada2)
+        {
+            int registros = 0;
+           string mensaje = "";
+     for(var i = 0; i < 3; i++)
+            {
+                if (i == 0) {
+                if (modelos.MontoAbono != 0)
+                {
+                    try
+                    {
+                        registros = modelo.AgregaRecibo(
+                            modelos.CodigoCliente,
+                            modelos.MontoAbono,
+                            modelos.NumeroFacturaAbonada
+
+                       );
+
+
+
+                    }
+
+                    catch (Exception e)
+                    {
+                        mensaje = "Ocurrió un error: " + e.Message;
+                    }
+
+                }
+                }
+                if (i == 1)
+                {
+
+                if (MontoAbono1 != 0)
+                {
+                    try
+                    {
+                        registros = modelo.AgregaRecibo(
+                            modelos.CodigoCliente,
+                            modelos.MontoAbono = (int)MontoAbono1,
+                            modelos.NumeroFacturaAbonada = (int)NumeroFacturaAbonada1
+
+                       );
+
+
+
+                    }
+
+                    catch (Exception e)
+                    {
+                        mensaje = "Ocurrió un error: " + e.Message;
+                    }
+
+                }
+                }
+                if (i == 2)
+                {
+                if (MontoAbono2 != 0)
+                {
+                    try
+                    {
+                        registros = modelo.AgregaRecibo(
+                            modelos.CodigoCliente,
+                            modelos.MontoAbono = (int)MontoAbono2,
+                            modelos.NumeroFacturaAbonada = (int)NumeroFacturaAbonada2
+
+                       );
+
+
+
+                    }
+
+                    catch (Exception e)
+                    {
+                        mensaje = "Ocurrió un error: " + e.Message;
+                    }
+
+                }
+                }
+            }
+              
+
+            return RedirectToAction("Pagos", "Home");
+        }
+
+
+
+
+
+        #endregion
+
+
+        public ActionResult Pagos()
+        {
+            return View();
+        }
+
+        public ActionResult Contact(int CodigoCliente)
+        {
+            ClienteID_Result modelovista = new ClienteID_Result();
+            modelovista = this.modelo.ClienteID(CodigoCliente).FirstOrDefault();
+            return View(modelovista);
+        }
+        [HttpPost]
+        public ActionResult Contact(ClienteID_Result modelos)
+        {
+            int registros = 0;
+
+            string mensaje = "";
+
+            try
+            {
+                registros = modelo.ModificarCliente(
+                    modelos.CodigoCliente,
+                    modelos.NombreCliente,
+                    modelos.SaldoMes,
+                    modelos.AbonoMes
+
+               );
+
+
+
+            }
+
+            catch (Exception e)
+            {
+                mensaje = "Ocurrió un error: " + e.Message;
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+        #region ingresaCliente
+        public ActionResult IngresarCliente()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult IngresarCliente(MuestraCliente_Result modelos)
+        {
+            int registros = 0;
+
+            string mensaje = "";
+
+            try
+            {
+                registros = modelo.InsertCliente(
+                    modelos.NombreCliente,
+                    modelos.SaldoMes,
+                    modelos.AbonoMes
+
+               );
+
+
+
+            }
+
+            catch (Exception e)
+            {
+                mensaje = "Ocurrió un error: " + e.Message;
+            }
+
+            return View();
+        }
+        #endregion
+
+    }
+}
---
+++ Facturacion/Facturacion/Scripts/Ajax/Data.js
@@ -0,0 +1,128 @@
+﻿$(function () {
+    estableceEventosChange();
+    cargaDropdownListCliente();
+});
+
+function estableceEventosChange() {
+    $("#Cliente").change(function () {
+        var Cliente = $("#Cliente").val();
+        cargaTabalaCliente(Cliente);
+    });
+}
+
+function cargaDropdownListCliente() {
+
+    var url = '/Home/RetornaClientes';
+    var parametros = {
+    };
+    $.ajax({
+        url: url,
+        dataType: 'json',
+        type: 'post',
+        contentType: 'application/json',
+        data: JSON.stringify(parametros),
+        success: function (data, textStatus, jQxhr) {
+            procesarResultadoCliente(data)
+        },
+        error: function (jQxhr, textStatus, errorThrown) {
+            alert(errorThrown);
+        },
+    });
+}
+function procesarResultadoCliente(data) {
+    var ddlCliente = $("#Cliente");
+    ddlCliente.empty();
+    var nuevaOpcion = "<option value=''>Seleccione un Cliente</option>";
+    ddlCliente.append(nuevaOpcion);
+    $(data).each(function () {
+        var ClienteActual = this;
+        nuevaOpcion = "<option value='" + ClienteActual.CodigoCliente + "'>" + ClienteActual.NombreCliente + "</option>";
+        ddlCliente.append(nuevaOpcion);
+    });
+}
+
+$(document).on('change', '#Cliente', function (event) {
+    $('#ID').val($("#Cliente option:selected").val());
+});
+
+///////////////////////////
+function cargaTabalaCliente(pCliente) {
+
+    var url = '/Home/RetornaFactura';
+    var parametros = {
+        CodigoCliente: pCliente
+    };
+    $.ajax({
+        url: url,
+        dataType: 'json',
+        type: 'post',
+        contentType: 'application/json',
+        data: JSON.stringify(parametros),
+        success: function (data, textStatus, jQxhr) {
+            procesarResultadoFactura(data)
+        },
+        error: function (jQxhr, textStatus, errorThrown) {
+            alert(errorThrown);
+        },
+    });
+}
+function eliminar(correlativo) {
+    $('#fila' + correlativo).remove();
+
+}
+var correlativo = 0;
+function procesarResultadoFactura(data) {
+    var ddlFactura = $("#Contenido");
+    ddlFactura.empty();
+    var nuevaOpcion = "";
+    ddlFactura.append(nuevaOpcion);
+    $(data).each(function () {
+        correlativo++;
+        var FacturaActual = this;
+        nuevaOpcion = "<tr><td id='fila" + correlativo + "'>" + FacturaActual.NumeroFactura + "</td><td>" + FacturaActual.CodigoCliente + "</td><td>" + FacturaActual.FechaFactura + "</td><td>" + FacturaActual.MontoOriginal + "</td><td>" + FacturaActual.MontoAbonos + "</td><td><input id='Dato" + correlativo + "'/></td></tr>"
+        ddlFactura.append(nuevaOpcion);
+    });
+}
+
+if ('#Dato1' != " ") {
+    $(document).on('change', '#Dato1', function (event) {
+        $("#IDs").show();
+        $('#IDs').val($("#Dato1").val());
+        var MyRows = $('table#conten').find('tbody').find('tr');
+        for (var i = 0; i < 2; i++) {
+            var MyIndexValue = $(MyRows[i]).find('td:eq(0)').html();
+        }
+        $('#IDc').val(MyIndexValue)
+    });
+
+
+} if ('#Dato2' != " ") {
+    $(document).on('change', '#Dato2', function (event) {
+        $("#ID1").show();
+        $('#ID1').val($("#Dato2").val());
+        var MyRows = $('table#conten').find('tbody').find('tr');
+        for (var i = 0; i < 3; i++) {
+            var MyIndexValue = $(MyRows[i]).find('td:eq(0)').html();
+        }
+        console.log(MyIndexValue)
+        $('#ID2').val(MyIndexValue)
+    });
+
+
+
+
+} if ('#Dato3' != " ") {
+    $(document).on('change', '#Dato3', function (event) {
+        $(".ID3").show();
+        $('#ID3').val($("#Dato3").val());
+        var MyRows = $('table#conten').find('tbody').find('tr');
+        for (var i = 0; i < 4; i++) {
+            var MyIndexValue = $(MyRows[i]).find('td:eq(0)').html();
+        }
+        console.log(MyIndexValue)
+        $('#ID4').val(MyIndexValue)
+    });
+ 
+}
+
+///////////////////////////
